d998e1362f670217fa09469cb9be20df47f9e71b
Chase <chasefreeman13@gmail.com>
2016-11-02 08:54:55 -0400

    object literals greeter

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
-var greet = require('./greet.js')
-greet();
+// var greet = require('./greet.js')
+// greet();
+
+var person = {
+  firstName: 'John',
+  lastName: 'Doe',
+  greet: function(){
+    console.log('Hello ' + this.firstName + ' ' + this.lastName);
+  }
+};
+person.greet();
