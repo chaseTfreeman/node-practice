86b566f8040d6e15128489b60e9cfdee38e62edd
Chase <chasefreeman13@gmail.com>
2016-11-02 09:12:49 -0400

    Constructors

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 // var greet = require('./greet.js')
 // greet();
 
-var person = {
-  firstName: 'John',
-  lastName: 'Doe',
-  greet: function(){
-    console.log('Hello ' + this.firstName + ' ' + this.lastName);
-  }
-};
-person.greet();
+// var person = {
+//   firstName: 'John',
+//   lastName: 'Doe',
+//   greet: function(){
+//     console.log('Hello ' + this.firstName + ' ' + this.lastName);
+//   }
+// };
+// // person.greet();
+// console.log(person['firstName'])
+
+function Person(firstName, lastName) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+}
+var John = new Person('John', 'Doe');
+console.log(John);
+console.log(John.firstName);
