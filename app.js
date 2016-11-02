7de392d25c02391076f929efc72226004bd937ec
Chase <chasefreeman13@gmail.com>
2016-11-02 09:27:11 -0400

    Protypical Inheritence

--- app.js
+++ app.js
@@ -15,6 +15,16 @@ function Person(firstName, lastName) {
   this.firstName = firstName;
   this.lastName = lastName;
 }
+
+Person.prototype.greet = function () {
+  console.log('Hello ' + this.firstName + ' ' + this.lastName);
+};
+
 var John = new Person('John', 'Doe');
-console.log(John);
-console.log(John.firstName);
+John.greet();
+
+var Jane = new Person('Jane', 'Doe');
+Jane.greet();
+console.log(John.__proto__);
+console.log(Jane.__proto__);
+console.log(John.__proto__ === Jane.__proto__);
