c7268bd3cddf1d22357a540d7e25004010fd56a0
Chase <chasefreeman13@gmail.com>
2016-11-10 11:28:21 -0500

    Cont'd Practice

--- app.js
+++ app.js
@@ -34,26 +34,34 @@
 // console.log(Jane.__proto__);
 // console.log(John.__proto__ === Jane.__proto__);
 
-//****** Pass by Value example
-function change(b){
-  b = 2;
-}
+// //****** Pass by Value example
+// function change(b){
+//   b = 2;
+// }
+//
+// var a = 1;
+// change(a);
+// // console.log(a); a remains = 1 becuase it is a primitive value and was unphased
+// // by the logic in the 'change function'(b points to the same memory address as a in this case)
+//
+// // ************ Pass by reference example
+// function changeObject(d){
+//   d.prop1 = function(){};
+//   d.prop2 = {};
+// }
+//
+// var c = {};
+// c.prop1 = {};
+//
+// changeObject(c);
+// console.log(c);
+// // In this case, c is changed FROM a function object to an object literal, THEN,
+//  // an additional empty object literal called prop2 is also added to the C object.
 
-var a = 1;
-change(a);
-// console.log(a); a remains = 1 becuase it is a primitive value and was unphased
-// by the logic in the 'change function'(b points to the same memory address as a in this case)
-
-// pass by reference example
-function changeObject(d){
-  d.prop1 = function(){};
-  d.prop2 = {};
-}
-
-var c = {};
-c.prop1 = {};
-
-changeObject(c);
-console.log(c);
-// In this case, c is changed FROM a function object to an object literal, THEN,
- // an additional empty object literal called prop2 is also added to the C object.
+// ********IIFEs
+(function(){
+  var firstName = 'John';
+  console.log(firstName);
+}());
+var firstName = 'Jane';
+console.log(firstName);
